ff3c46378e1ee5566ab110e8c0dd4943119f4846
Faraoman <faraoman@live.it>
2020-04-04 20:32:53 +0200

    SwitchHosts! script

---
+++ Manjaro/switchosts_installer_3.5.4.5517.sh
@@ -0,0 +1,67 @@
+GetCurrentScriptName () {
+    echo "$(basename "$0")"
+}
+
+GetCurrentExecutionDirectory() {
+    echo "$( cd "$(dirname "$0")" ; pwd -P )"
+}
+
+SplitString () {
+    readarray -d $1 -t result <<< "$2"
+    echo "${result[*]}"
+}
+
+ArrayJoin () { local IFS="$1"; shift; echo "$*"; }
+
+current_script_name=$(GetCurrentScriptName) # switchost_installer_3.5.4.5517.sh
+readarray -d _ -t splitted <<< $(GetCurrentScriptName) # switchost installer 3.5.4.5517.sh
+strFullVersionSH=${splitted[$((${#splitted[*]}-1))]} # 3.5.4.5517.sh
+readarray -d . -t aoVersionSH <<< $strFullVersionSH # 3 5 4 5517 sh
+aoFullVersion=${aoVersionSH[@]:0:$((${#aoVersionSH[*]}-1))} # 3 5 4 5517
+strFullVersion=$(ArrayJoin "." $aoFullVersion) # 3.5.4.5517
+aoMainVersion=${aoVersionSH[@]:0:$((${#aoVersionSH[*]}-2))} # 3 5 4
+strMainVersion=$(ArrayJoin "." $aoMainVersion) # 3.5.4
+
+# download favicon
+if [ ! -f favicon.png ]; then
+    curl -L https://oldj.github.io/SwitchHosts/images/favicon.png >favicon.png
+fi
+
+# download SwitchHosts
+if [ ! -f SwitchHosts.zip ]; then
+    curl -L https://github.com/oldj/SwitchHosts/releases/download/v$strMainVersion/SwitchHosts._linux_x64_$strFullVersion.zip >SwitchHosts.zip
+fi
+
+APPLICATIONS_PATH=$HOME/Applications
+SWITCHHOSTS_PATH=$APPLICATIONS_PATH/SwitchHosts
+
+if [ ! -d $APPLICATIONS_PATH ]; then
+    mkdir $APPLICATIONS_PATH
+fi
+
+if [ ! -d $SWITCHHOSTS_PATH ]; then
+    mkdir $SWITCHHOSTS_PATH
+fi
+
+unzip -q SwitchHosts.zip -d $SWITCHHOSTS_PATH
+
+mv favicon.png $SWITCHHOSTS_PATH
+rm SwitchHosts.zip
+
+if [ ! -d $HOME/Desktop/SwitchHosts ]; then
+    # creating dekstopfile
+    echo "[Desktop Entry]" >>$HOME/Desktop/SwitchHosts
+    echo "Comment=" >>$HOME/Desktop/SwitchHosts
+    echo "Exec=$SWITCHHOSTS_PATH/switchhosts" >>$HOME/Desktop/SwitchHosts
+    echo "GenericName=" >>$HOME/Desktop/SwitchHosts
+    echo "Icon=$SWITCHHOSTS_PATH/favicon.png" >>$HOME/Desktop/SwitchHosts
+    echo "Name=SwitchHosts" >>$HOME/Desktop/SwitchHosts
+    echo "Path=$SWITCHHOSTS_PATH" >>$HOME/Desktop/SwitchHosts
+    echo "StartupNotify=true" >>$HOME/Desktop/SwitchHosts
+    echo "Terminal=false" >>$HOME/Desktop/SwitchHosts
+    echo "TerminalOptions=" >>$HOME/Desktop/SwitchHosts
+    echo "Type=Application" >>$HOME/Desktop/SwitchHosts
+    chmod +x $HOME/Desktop/SwitchHosts
+fi
+
+echo "Installed!"
