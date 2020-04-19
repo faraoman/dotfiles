74b149b1936e8f88ba916fd8f5a7d2ef70017e97
Faraoman <faraoman@live.it>
2020-04-19 14:35:42 +0200

    added 3 new apps

--- Manjaro/2.install_aur.sh
+++ Manjaro/2.install_aur.sh
@@ -3,7 +3,7 @@
 # Install Packages
 InstallPacks() {
 gpg --auto-key-locate nodefault,wkd --locate-keys torbrowser@torproject.org
-toInstall=(tor-browser anydek visual-studio-code-bin github-desktop-git google-chrome teamviewer chromium-vaapi-bin jdownloader2 lutris-git)
+toInstall=(tor-browser anydek visual-studio-code-bin github-desktop-git google-chrome teamviewer chromium-vaapi-bin jdownloader2 lutris-git ksnip joplin stacer-bin)
 for pack in ${toInstall[*]}
 do
     yay -Qs $pack /dev/null 2>&1
